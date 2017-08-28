429edd2b767a937a9ccb62f766141c65d669a845
David Green <david.green@cd-adapco.com>
2017-08-28 14:25:00 -0400

    allow localhost

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,7 +12,7 @@ import environ
 ROOT_DIR = environ.Path(__file__) - 3  # (vmsareus/config/settings/base.py - 3 = vmsareus/)
 APPS_DIR = ROOT_DIR.path('vmsareus')
 
-ALLOWED_HOSTS = ['vmsareus.lebanon.cd-adapco.com', 'vmsareus']
+ALLOWED_HOSTS = ['vmsareus.lebanon.cd-adapco.com', 'vmsareus', '127.0.0.1']
 
 # Load operating system environment variables and then prepare to use them
 env = environ.Env()
