553d40440a5491285a8c1302f5f4bbbfda21f6ab
David Green <david.green@cd-adapco.com>
2017-10-17 17:52:30 -0400

    git setup commands

--- vmsareus/vmleases/urls.py
+++ vmsareus/vmleases/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.vm_list, name='vm_list'),
-    url(r'^vm/(?P<pk>\d+)/$', views.vm_detail, name='vm_detail'),
     url(r'^vm/new/$', views.vm_new, name='vm_new'),
+    url(r'^vm/(?P<pk>\d+)/manage/$', views.vm_manage, name='vm_manage'),
+    url(r'^vm/(?P<pk>\d+)/$', views.vm_detail, name='vm_detail'),
+
     url(r'^vm/(?P<pk>\d+)/remove/$', views.vm_remove, name='vm_remove'),
     url(r'^vm/(?P<pk>\d+)/extend/$', views.vm_extend, name='vm_extend'),
-    url(r'^vm/(?P<pk>\d+)/manage/$', views.vm_manage, name='vm_manage'),
 ]
